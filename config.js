56c73ac0efa6ebd99bcc21ccdee088feddc878ff
asdqwefz <crownesxdd@gmail.com>
2026-08-26 03:55:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,6 @@ const CONFIG = {
         username: '@crownesxd',
         avatar: 'https://media.discordapp.net/attachments/1468015739737739494/1541925463008612483/29db5b0552d85ee78c7b6beffcd97d5f.jpg?ex=6a8f5d7d&is=6a8e0bfd&hm=a0012bbdccdcfc45b986ffb02582f0fadbeea7def9e3653c8f277ae573c13b8c&=&format=webp', // veya kendi resim linkin / dosya yolun
         status: 'online', // 'online', 'idle', 'dnd', 'offline'
-        customStatusText: 'professional', // Fotoğraftaki durum balonu
         socials: {
             pinterest: 'https://www.pinterest.com/crownesxd/'
         }
@@ -25,7 +24,6 @@ const CONFIG = {
         username: '@toxicluvs',
         avatar: 'https://media.discordapp.net/attachments/1468015739737739494/1541925463008612483/29db5b0552d85ee78c7b6beffcd97d5f.jpg?ex=6a8f5d7d&is=6a8e0bfd&hm=a0012bbdccdcfc45b986ffb02582f0fadbeea7def9e3653c8f277ae573c13b8c&=&format=webp', // veya kendi resim linkin / dosya yolun
         status: 'online',
-        customStatusText: 'vanilya',
         socials: {
             pinterest: 'https://www.pinterest.com/pinnershin/'
         }
